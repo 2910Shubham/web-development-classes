923964e19a32300840f09f44919cabf501892839
2910Shubham <2910viwan@gmail.com>
2026-09-19 15:04:40 +0530

    butterfly pattern

--- c++/main.cpp
+++ c++/main.cpp
@@ -3,10 +3,40 @@ using namespace std;
 
 int main(){
     int a = 5;
+
+    //upper
+
      for(int i=1; i<=a; i++){
         for(int j=1;j<=i;j++){
             cout << "*";
         }
+        for(int j=a-i; j>=1; j--){
+            cout << " " << " " ;
+        }
+         for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+    
         cout << "\n";
      }
+
+
+     //lower 
+
+     for(int i=1; i<=a; i++){
+        for(int j=a-i;j>=1;j--){
+            cout << "*";
+        }
+        for(int j=1; j<=i; j++){
+            cout << " " << " ";
+        }
+          for(int j=a-i;j>=1;j--){
+            cout << "*";
+        }
+    
+        cout << "\n";
+     }
+
+
+
 }
