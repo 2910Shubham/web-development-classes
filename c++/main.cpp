dbabf84a24fd2259304e577fa9e9a3d166c0288f
2910Shubham <2910viwan@gmail.com>
2026-09-19 12:01:13 +0530

    hollow doamond

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,17 +1,68 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
-    int a =4, count=0;
-    for(int i=0; i<=a-1; i++){
-        for(int k=1; k<=a-i; k++){
+    int a = 4;
+
+    // Top Part
+    for(int i = 1; i <= a; i++) {
+        // Leading spaces
+        for(int j = a - i; j >= 1; j--) {
             cout << " ";
         }
-        for(int j=0;j<=i;j++){
-            cout <<"*" << " ";
+
+        // Left side numbers
+        for(int j = 1; j <= i; j++) {
+            if(j>1){
+                cout << " ";
+            }else{
+
+                cout << "*";
+            }
         }
+
+        // Right side numbers
+        for(int j = i - 1; j >= 1; j--) {
+           if(j>1){
+                cout << " ";
+            }else{
+
+                cout << "*";
+            }
+        }
+
         cout << "\n";
     }
- return 0;
+
+    // Bottom Part
+    for(int i = a - 1; i >= 1; i--) {
+        // Leading spaces
+        for(int j = a - i; j >= 1; j--) {
+            cout << " ";
+        }
+
+        // Left side numbers
+        for(int j = 1; j <= i; j++) {
+            if(j>1){
+                cout << " ";
+            }else{
+
+                cout << "*";
+            }
+        }
+
+        // Right side numbers
+        for(int j = i - 1; j >= 1; j--) {
+          if(j>1){
+                cout << " ";
+            }else{
+
+                cout << "*";
+            }
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
 }
