bcabb7138832b6ea2568a12285743b8914e72a0e
2910Shubham <2910viwan@gmail.com>
2026-09-20 19:44:36 +0530

    array swaping

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,70 +1,22 @@
 #include <iostream>
-#include <climits>
+#include <array>
 using namespace std;
 
-int fn(int arr[], int size){
-    int max = INT_MIN;
-    for(int i=0; i<size; i++){
-        if(arr[i]>max){
-            max = arr[i];
-        }
+int main(){
+    int arr[8] = {2, 5, 6, 3, 4, 8, 9, 7};
+    int size = sizeof(arr) / sizeof(int);
+    int start = 0;
+    int end = size -1;
+
+    for(start,end; start<end; start++, end--)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end --;
+       
     }
 
-    return max;
-}
-
-int min(int arr[], int size){
-    int min= INT_MAX;
-
     for(int i=0; i<size; i++){
-        if(min>arr[i]){
-            min = arr[i];
-        }
+        cout << arr[i];
     }
-    return min;
-
-}
-
-int sum(int arr[], int size){
-    int add=0;
-
-    for(int i=0; i<size; i++){
-        add += arr[i];
-    }
-    return add;
-}
-
-
-int main() {
-   // cout << nCr(8,2);
-     int size;
-    cout << "Enter the size of array:";
-    cin >>size;
-
-    int arr[size];
-
-    cout << "Enter the " << size << " numbers";
-
-    for(int i=0; i<size; i++){
-        cin >> arr[i];
-    }
-     cout << "your entered numbers are:";
-    for(int i=0; i<size; i++){
-
-
-        cout << arr[i] << "\n";
-    }
-
-     cout << "max value is:" << fn(arr,size) << "\n";
-     cout << "min value is" << min(arr,size) << "\n";
-     cout << "sum of all value is" << sum(arr,size) << "\n";
-
-
-
-   
-
-
-
-    return 0;
-
 }
